0526400d3ecaf35af48b8b1b8c8d5ecde2095308
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-03-25 03:08:59 +0700

    commit 4: add page (phase 2)

--- lib/app.dart
+++ lib/app.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:taskhub_app/pages/add_note_page.dart';
 import 'package:taskhub_app/providers/note_provider.dart';
-
-// import '../pages/login_page.dart';
 import './providers/user_provider.dart';
 
 class App extends StatelessWidget {
